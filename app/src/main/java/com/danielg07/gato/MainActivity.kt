85685d691b70ae2bbbfa2a0d53dc5407fe41bd44
Daniel Gonzalez <enda0507@gmail.com>
2021-11-25 22:56:38 -0600

    Initial commit

---
+++ app/src/main/java/com/danielg07/gato/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.danielg07.gato
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
