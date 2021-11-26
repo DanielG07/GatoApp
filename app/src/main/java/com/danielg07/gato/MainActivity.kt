96340cdb74f0824168cb78a8bcc0fd8be974a1f2
Daniel Gonzalez <enda0507@gmail.com>
2021-11-26 01:53:32 -0600

    Creacion de layout principal y configuracion, asi como su navegacion.

--- app/src/main/java/com/danielg07/gato/MainActivity.kt
+++ app/src/main/java/com/danielg07/gato/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.danielg07.gato
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun playButtonClick(view: View){
+        var intent = Intent(this, PlayerSetup::class.java);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/danielg07/gato/PlayerSetup.kt
@@ -0,0 +1,11 @@
+package com.danielg07.gato
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerSetup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.player_setup)
+    }
+}
